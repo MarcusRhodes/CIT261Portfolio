83a90d92eb422b64bbbad6febdc9b7fa3dd6cd8e
marcusBYUI <rho15005@byui.edu>
2018-12-02 22:30:19 -0700

    Trying to get SnapThat on github.

--- Portfolio/script2.js
+++ Portfolio/script2.js
@@ -5,16 +5,19 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || 
-    navigator.mozGetUserMedia || navigator.oGetUserMedia || navigator.msGetUserMedia;
+    navigator.mozGetUserMedia || navigator.oGetUserMedia || navigator.msGetUserMedia;//navigator.mediaDevices.getUserMedia
 
 if (navigator.getUserMedia) {
     navigator.getUserMedia({video:true}, streamWebCam, throwError);
 }
 
 function streamWebCam(stream) {
-    //console.dir(stream);
-    video.src = stream;//window.URL.createObjectURL(stream);//stream;
-    video.play();//This isn't working:...
+    try {
+        video.srcObject = stream;
+    } catch (error) {
+        video.src = window.URL.createObjectURL(stream);
+    }
+    video.play();
 }
 
 function throwError (e) {
@@ -25,10 +28,27 @@ function throwError (e) {
 function snap() {
     canvas.width = video.clientWidth;
     canvas.height = video.clientHeight;
-    canvas.drawImage(video, 0, 0);
+
+    var context = canvas.getContext("2d");
+
+    if (video.paused) video.play();
+    else video.pause();
+
+    context.drawImage(video, 0, 0);
+
+    if (video.style.display != "none") {
+        video.style.display = "none";
+        document.getElementById("filter").style.diplay = "none";
+        document.getElementById("share").style.diplay = "none";
+    } else {
+        video.style.display = "inline";
+        document.getElementById("filter").style.diplay = "inline";
+        document.getElementById("share").style.diplay = "inline";
+    }
 }
 
 function filter() {
+
     return;
 }
 
